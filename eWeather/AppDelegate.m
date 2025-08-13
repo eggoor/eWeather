6a832182a393bd20e12c585c7ba9953947d6b5ab
eggor <egggor@gmail.com>
2025-08-13 10:08:21 +0300

    Initial Commit

---
+++ eWeather/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  eWeather
+//
+//  Created by e on 13.08.2025.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+	return YES;
+}
+
+
+@end
---
+++ eWeatherUITests/eWeatherUITestsLaunchTests.m
@@ -0,0 +1,37 @@
+//
+//  eWeatherUITestsLaunchTests.m
+//  eWeatherUITests
+//
+//  Created by e on 13.08.2025.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface eWeatherUITestsLaunchTests : XCTestCase
+
+@end
+
+@implementation eWeatherUITestsLaunchTests
+
++ (BOOL)runsForEachTargetApplicationUIConfiguration {
+    return YES;
+}
+
+- (void)setUp {
+    self.continueAfterFailure = NO;
+}
+
+- (void)testLaunch {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    // Insert steps here to perform after app launch but before taking a screenshot,
+    // such as logging into a test account or navigating somewhere in the app
+
+    XCTAttachment *attachment = [XCTAttachment attachmentWithScreenshot:XCUIScreen.mainScreen.screenshot];
+    attachment.name = @"Launch Screen";
+    attachment.lifetime = XCTAttachmentLifetimeKeepAlways;
+    [self addAttachment:attachment];
+}
+
+@end
